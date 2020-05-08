4cf1aedb6d5789ad309ffb0b1261368bdfa387c1
Lourenço Passos <lo.passos93@gmail.com>
2020-05-08 14:54:56 -0300

    exercícios de interpretação

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,13 @@
+// Exercício 1
+// A função transforma o valor de uma moeda em dólar. 
+// Pede para o usuário digitar através do prompt e utiliza a função Number pra converter de sting para número.
+// Depois da conversão, printa no console o valor convertido em reais.
+
+// Exercício 2
+// A função investeDinheiro calcula o valorAposInvestimento conforme o parametro tipo de investimento do switch case. Depois disso vai ter o cálculo de dois montantes, só que o do "tesouro direto" vai dar erro, pois não há esse tipo de investimento.
+
+// Exercício 3
+// O for tem uma função que verifica se o número é par. Se for coloca no array1, caso contrário no 2. Depois ele ve o tamanho do array inicial "numeros" e depois dois dois novos arrays formados.
+
+//Exercício 4
+// Para cada número dentro do array números. O primeiro if não sei dizer, diria que vai dar um undefined por causa do infinity. Já o segundo, vai printar os números abaixo de zero, no caso o -10.
