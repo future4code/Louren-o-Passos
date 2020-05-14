db6edc795194e3a772bb1d1ae136728e037a9c47
Lourenço Passos <lo.passos93@gmail.com>
2020-05-14 17:03:40 -0300

    exercicio incompleto

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,28 +3,53 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150/'}
-        />
-        <Post
-          nomeUsuario={'Soter'}
-          fotoUsuario={'https://picsum.photos/50/50?random=1'}
-          fotoPost={'https://picsum.photos/200/150?random=1'}
-        />
-                <Post
-          nomeUsuario={'Pedro'}
-          fotoUsuario={'https://picsum.photos/50/50?random=2'}
-          fotoPost={'https://picsum.photos/200/150?random=2'}
-        />
-      </div>
-      
-    );
-  }
-}
+  state = {
 
+    usuarios : [
+    {
+    nome: "paulinha",
+    foto: "https://picsum.photos/50/50",
+    fotoPost: "https://picsum.photos/200/150/"
+  },
+  {
+    nome: "Soter",
+    foto: "https://picsum.photos/50/50?random=1",
+    fotoPost: "https://picsum.photos/200/150/?random=1"
+
+  },
+  {
+    nome: "Pedro",
+    foto: "https://picsum.photos/50/50?random=2",
+    fotoPost: "https://picsum.photos/200/150/?random=2"
+
+  }
+  ]
+};
+
+
+  render() {
+
+    const listaUsuarios = this.state.usuarios.map((usuario) => {
+       
+      return (
+       
+        <div className={'app-container'}>
+          <Post
+          nomeUsuario={usuario.nome}
+          fotoUsuario={usuario.foto}
+          fotoPost={usuario.fotoPost}
+          />
+        </div>  
+
+    );
+  });
+
+
+  return (
+    <div>
+      <div>{listaUsuarios}</div>
+    </div>
+  );
+}
+}
 export default App;
