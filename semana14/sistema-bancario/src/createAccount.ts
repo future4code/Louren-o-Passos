429d61ab6f0e2465cb028b7c4dcd5b3a6003c9e7
Lourenço Passos <lo.passos93@gmail.com>
2020-07-31 18:00:57 -0300

    correção

--- semana14/sistema-bancario/src/createAccount.ts
+++ semana14/sistema-bancario/src/createAccount.ts
@@ -10,6 +10,7 @@ const userBirth: string = process.argv[4];
 const accounts: string = fs.readFileSync("./data.json").toString();
 
 const updatedAccounts: User[] = accounts ? JSON.parse(accounts) : [];
+console.log(updatedAccounts);
 
 const birthday: moment.Moment = moment(userBirth, "DD/MM/YYYY");
 
@@ -21,17 +22,18 @@ const createAccount = (
   try {
     const diff = moment().diff(birthday, "years");
     if (diff >= 18) {
-      // const user = {
-      //   name,
-      //   birthday,
-      //   cpf,
-      // };
-      // updatedAccounts.push(user);
-      // const data = JSON.stringify(updatedAccounts, null, 2);
-      // fs.writeFileSync("./data.json", data);
-      // console.log("Conta criada com sucesso!");
+      const user = {
+        name,
+        birthday,
+        cpf,
+      };
+      updatedAccounts.push(user);
+      const data = JSON.stringify(updatedAccounts, null, 2);
+      fs.writeFileSync("./data.json", data);
+      console.log("Conta criada com sucesso!");
     } else {
       console.log("Somente permitida criação de conta acima de 18 anos");
+      return;
     }
   } catch (error) {
     console.log(error.message);
